476a300ecff1588f32ff2c8d9fa6e27f71c1cae7
nithyaniths <nithyapramod97@gmail.com>
2024-11-27 16:04:47 +0530

    final

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -135,9 +135,9 @@ EMAIL_HOST_PASSWORD = 'zjjd cjmi qtaq ncwi'
 
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+#STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
