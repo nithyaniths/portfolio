6ae74c628f28dc1f805e0770e3ae02692cff0bd4
nithyaniths <nithyapramod97@gmail.com>
2024-11-27 13:17:16 +0530

    new_updation

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2ngy1%laduy!7lo)pea9baqf8u5mbqsw$=daxu##mdk*4&tot=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -134,6 +134,7 @@ EMAIL_HOST_PASSWORD = 'zjjd cjmi qtaq ncwi'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
